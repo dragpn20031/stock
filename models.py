dcb72577efd91d79ead8e681f74ced0aa0edad1e
dragpn20031 <idriselmaimouni@gmail.com>
2024-11-26 12:24:45 +0100

    models.py

--- models.py
+++ models.py
@@ -1,29 +1,24 @@
+# models.py
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
-
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def __repr__(self):
+        return f'<User {self.username}>'
 
 class StockItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    quantity = db.Column(db.Integer, default=0)
-    pack_size = db.Column(db.Integer, default=1)
-    site_id = db.Column(db.Integer, nullable=False)
-
-
-class StockLog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    action = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    pack = db.Column(db.Boolean, default=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    stock_item_id = db.Column(db.Integer,
-                              db.ForeignKey('stock_item.id'),
-                              nullable=False)
+    site_id = db.Column(db.String(100), nullable=False)  # Each site has its own stock
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<StockItem {self.name}, Quantity {self.quantity}>'
